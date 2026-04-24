2c9c7124238dfe7dc294b21c572c82457194aa66
floofytech-public-projects <floofytechf0@gmail.com>
2026-04-24 14:59:53 -0400

    Modify sleep duration and output messages in floofnugget.rb

--- floofnugget.rb
+++ floofnugget.rb
@@ -1,3 +1,5 @@
 puts "you can boop them\n"
-sleep 12
-puts "awooooooo\n -from lydia and all the hoomans at floofytech"
+sleep 2
+puts "boop!"
+sleep 5
+puts "awoooooo\n -from lydia and all the hoomans at floofytech"
